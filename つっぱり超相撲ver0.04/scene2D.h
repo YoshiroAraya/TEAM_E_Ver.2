f468f8541c9737766d272d8684e95b184244b306
MeguroMikiya <megumiki0718@gmail.com>
2019-05-17 11:05:20 +0900

    上げ忘れ

--- つっぱり超相撲ver0.04/scene2D.h
+++ つっぱり超相撲ver0.04/scene2D.h
@@ -13,7 +13,7 @@
 //*****************************************************************************
 // マクロ定義
 //*****************************************************************************
-#define PLAYER_NAME		"data/TEXTURE/.png"	// プレイヤーのテクスチャ名
+#define PLAYER_NAME		"data/TEXTURE/player000.png"	// プレイヤーのテクスチャ名
 #define BULLET_NAME		"data/TEXTURE/bullet000.png"	// 弾のテクスチャ名
 #define MAX_TEX_TYPE	(2)								// テクスチャの種類
 
@@ -38,12 +38,25 @@ public:
 	void SetPosition(D3DXVECTOR3 pos);					// 位置を設定
 
 	static CScene2D *Create(D3DXVECTOR3 pos);			// オブジェクトの生成
+	void BindTexture(LPDIRECT3DTEXTURE9 pTexture);
+	void SetPos(D3DXVECTOR3 pos, float fSpin, float fScale, D3DXCOLOR col);
+	void SetRot(float fSpin);
+	void SetWidthHeight(float fWidth, float fHeight);
+	void SetRIghtLeft(float fRight, float fLeft);
+	void SetCol(D3DXCOLOR col);
 
 private:
 	LPDIRECT3DTEXTURE9		m_pTexture;					// テクスチャへのポインタ
 	LPDIRECT3DVERTEXBUFFER9 m_pVtxBuff;					// 頂点バッファへのポインタ
 
 	D3DXVECTOR3				m_Pos;						// ポリゴンの位置
+	float					m_fSpin;				// 回転
+	float					m_fWidth, m_fHeight;	// 幅高さ
+	float					m_fRight, m_fLeft;		// 左右の長さ
+	float					m_fLength;				// 大きさ
+	float					m_fScale;				// 大きさ変更
+	D3DXCOLOR				m_Col;
+
 };
 
 #endif
