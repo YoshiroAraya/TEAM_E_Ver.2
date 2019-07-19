7faf293477403936dbebd99b915d7f66ec921c01
長山拓実 <taku1002na@gmail.com>
2019-07-19 11:56:10 +0900

    テクスチャいじった

--- つっぱり超相撲ver0.2/charaSelect.cpp
+++ つっぱり超相撲ver0.2/charaSelect.cpp
@@ -101,72 +101,31 @@ void CCharaSelect::Uninit(void)
 //=============================================================================
 void CCharaSelect::Update(void)
 {
-	//// 入力情報を取得
-	//CInputKeyboard *pInputKeyboard;
-	//pInputKeyboard = CManager::GetInputKeyboard();
-	//CXInputJoyPad *pXInput = NULL;
-	//pXInput = CManager::GetXInput();
+	// 入力情報を取得
+	CInputKeyboard *pInputKeyboard;
+	pInputKeyboard = CManager::GetInputKeyboard();
+	CXInputJoyPad *pXInput = NULL;
+	pXInput = CManager::GetXInput();
 
-	//LPDIRECT3DVERTEXBUFFER9 pVtxBuff;
-	//pVtxBuff = CScene2D::GetBuff();
+	LPDIRECT3DVERTEXBUFFER9 pVtxBuff;
+	pVtxBuff = CScene2D::GetBuff();
 
-	//D3DXVECTOR3 pos = CScene2D::GetPosition();
+	D3DXVECTOR3 pos = CScene2D::GetPosition();
 
-	//VERTEX_2D *pVtx;	// 頂点情報へのポインタ
+	VERTEX_2D *pVtx;	// 頂点情報へのポインタ
 
-	//					// 頂点バッファをロックし、頂点データへのポインタを取得
-	//pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
+						// 頂点バッファをロックし、頂点データへのポインタを取得
+	pVtxBuff->Lock(0, 0, (void**)&pVtx, 0);
 
-	//if (CCamera::GetState() == CCamera::STATE_HIGASHI)
-	//{
-	//	// テクスチャ座標の設定
-	//	pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
-	//	pVtx[1].tex = D3DXVECTOR2(0.5f, 0.0f);
-	//	pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
-	//	pVtx[3].tex = D3DXVECTOR2(0.5f, 1.0f);
 
-	//	if (m_fScale <= TOUZAI_SIZE)
-	//	{
-	//		m_fScale += 5.0f;
+	// テクスチャ座標の設定
+	pVtx[0].tex = D3DXVECTOR2(0.0f, 0.0f);
+	pVtx[1].tex = D3DXVECTOR2(0.5f, 0.0f);
+	pVtx[2].tex = D3DXVECTOR2(0.0f, 1.0f);
+	pVtx[3].tex = D3DXVECTOR2(0.5f, 1.0f);
 
-	//		if (m_fScale > TOUZAI_SIZE)
-	//		{
-	//			m_fScale = TOUZAI_SIZE;
-	//		}
-	//	}
-
-	//	pos = D3DXVECTOR3(200.0f, 150.0f, 0.0f);
-	//}
-	//else if (CCamera::GetState() == CCamera::STATE_NISHI)
-	//{
-	//	if (m_bReset == true)
-	//	{
-	//		m_fScale = 10.0f;
-
-	//		m_bReset = false;
-	//	}
-
-	//	// テクスチャ座標の設定
-	//	pVtx[0].tex = D3DXVECTOR2(0.5f, 0.0f);
-	//	pVtx[1].tex = D3DXVECTOR2(1.0f, 0.0f);
-	//	pVtx[2].tex = D3DXVECTOR2(0.5f, 1.0f);
-	//	pVtx[3].tex = D3DXVECTOR2(1.0f, 1.0f);
-
-	//	if (m_fScale <= TOUZAI_SIZE)
-	//	{
-	//		m_fScale += 5.0f;
-
-	//		if (m_fScale > TOUZAI_SIZE)
-	//		{
-	//			m_fScale = TOUZAI_SIZE;
-	//		}
-	//	}
-
-	//	pos = D3DXVECTOR3(SCREEN_WIDTH - 200.0f, 150.0f, 0.0f);
-	//}
-
-	//CScene2D::SetBuff(pVtxBuff);
-	//CScene2D::SetPos(pos, 0.0f, m_fScale, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
+	CScene2D::SetBuff(pVtxBuff);
+	CScene2D::SetPos(pos, 0.0f, m_fScale, D3DXCOLOR(1.0f, 1.0f, 1.0f, 1.0f));
 }
 
 //=============================================================================
